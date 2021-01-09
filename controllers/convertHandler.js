cd9b291df5b5514304f6d36eb7b8f1925387d270
Glitch (fcc---metric---imperial---converter) <none>
2021-01-09 18:42:16 +0000

    🏵⚽️ Updated with Glitch

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -8,7 +8,7 @@
 
 function ConvertHandler() {
   //   function to the nuber from input
-  this.getNum = function (input) {
+  this.getNum = function(input) {
     const regexSplit = /[a-z]/i;
     let result;
     if (regexSplit.test(input) === true) {
@@ -20,22 +20,22 @@ function ConvertHandler() {
     const regexNum = /[1-9]\d*(\.\d+)?\/?[1-9]?(\d*)?(\.\d+)?/;
     const regexDoubleFraction = /\//g;
 
-    if (result === '') {
+    if (result === "") {
       result = 1;
       return result;
     }
 
     if (regexNum.test(result) === false) {
-      result = 'invalid number';
+      result = "invalid number";
     } else if (
       result.match(regexDoubleFraction) !== null &&
       result.match(regexDoubleFraction).length >= 2
     ) {
-      result = 'invalid number';
+      result = "invalid number";
     } else {
       result = eval(result);
       const resultString = result.toString();
-      const index = resultString.indexOf('.');
+      const index = resultString.indexOf(".");
       if (resultString.length - index - 1 > 5) {
         result = parseFloat(result.toFixed(5));
       }
@@ -53,13 +53,13 @@ function ConvertHandler() {
     // slice from the first index of aplhabet to end of input and change it to be lowercase
     result = input.slice(indexOfAlpha).toLowerCase();
 
-
     // if there is space in unit
-    if (["gal", "l", "mi", "km", "lbs", "kg"].indexOf(result)===-1) return "invalid unit";
+    if (["gal", "l", "mi", "km", "lbs", "kg"].indexOf(result) === -1)
+      return "invalid unit";
 
     // for liter
-    if(result==="l") return "L";
-    
+    if (result === "l") return "L";
+
     return result;
   };
 
@@ -89,23 +89,23 @@ function ConvertHandler() {
   this.convert = function(initNum, initUnit) {
     let result;
     const galToL = 3.78541;
-    const lToGal = 1/galToL;
+    const lToGal = 1 / galToL;
     const lbsToKg = 0.453592;
-    const kgToLbs = 1/lbsToKg;
+    const kgToLbs = 1 / lbsToKg;
     const miToKm = 1.60934;
-    const kmToMi = 1/miToKm;
+    const kmToMi = 1 / miToKm;
     let input = ["gal", "L", "mi", "km", "lbs", "kg"];
     let convert = [galToL, lToGal, miToKm, kmToMi, lbsToKg, kgToLbs];
     result = initNum * convert[input.indexOf(initUnit)];
-    
-    if (result !== 'invalid number') {
+
+    if (result !== "invalid number") {
       const resultString = result.toString();
-      const index = resultString.indexOf('.');
+      const index = resultString.indexOf(".");
       if (resultString.length - index - 1 > 5) {
         result = Number.parseFloat(result.toFixed(5));
       }
     }
-    
+
     return result;
   };
 
